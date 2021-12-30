7f1845e17029b4cb706b9dd7eb13afe5d4af79ea
Zimovik007 <programm00@ya.ru>
2021-12-30 13:14:58 +0300

    fix bug with path and packaging disposable_emails file

--- disposable_email_validator/validator.py
+++ disposable_email_validator/validator.py
@@ -1,8 +1,11 @@
+import os
 import pickle
 
 
 def is_disposable_email(email: str) -> bool:
     email = email.split('@')[-1]
 
-    with open('disposable_email_validator/disposable_emails', 'rb') as file:
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+    with open(os.path.join(__location__, "disposable_emails"), 'rb') as file:
         return email in pickle.load(file)
