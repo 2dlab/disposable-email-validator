693a90d6c01ea8c47b9dcd3c609be58393bcce8d
Zimovik007 <programm00@ya.ru>
2021-12-30 11:42:35 +0300

    fix path to disposable_emails

--- disposable_email_validator/validator.py
+++ disposable_email_validator/validator.py
@@ -4,5 +4,5 @@ import pickle
 def is_disposable_email(email: str) -> bool:
     email = email.split('@')[-1]
 
-    with open('disposable_emails', 'rb') as file:
+    with open('disposable_email_validator/disposable_emails', 'rb') as file:
         return email in pickle.load(file)
